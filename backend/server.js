ecc2ec5b6b718bb50fe06ce5c107f3fd27c4e768
guilhermeDaviDev <guilhermedavidev@gmail.com>
2026-03-22 13:57:06 -0300

    saved changes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import "dotenv/config"
 import cors from 'cors'          // ← adicione
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "./generated/prisma/client.js"
 
 const prisma = new PrismaClient()
 const app = express()
