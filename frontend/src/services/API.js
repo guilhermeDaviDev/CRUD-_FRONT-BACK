ef3477df8e26925d8e15a99f31c722cc26679757
guilhermeDaviDev <guilhermedavidev@gmail.com>
2026-03-22 13:13:55 -0300

    Update API base URL for production environment

--- frontend/src/services/API.js
+++ frontend/src/services/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'https://sua-url-do-railway.up.railway.app'
+    baseURL: 'crud-front-back-production.up.railway.app'
 })
     // export default API
  
